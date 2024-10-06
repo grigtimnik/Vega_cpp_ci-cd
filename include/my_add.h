d30ea34a298eaad9069aa9dc0021c60ced375bb3
grigtimnik <grigtimnik@mail.ru>
2024-10-06 20:08:29 +0300

    add doxygen

--- include/my_add.h
+++ include/my_add.h
@@ -1,3 +1,6 @@
+/*!
+Header for my_add.cpp
+ */
 #pragma once
 
 int my_add(int, int);
--- src/my_add.cpp
+++ src/my_add.cpp
@@ -1,1 +1,6 @@
+/*!
+Adding 2 integer numbers
+ 
+Return type: int
+ */
 int my_add(int a, int b) { return a + b; }
